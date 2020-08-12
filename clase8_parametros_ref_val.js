a88a173763e437ec60a560c52ec6adc5515e14e5
CurtoBrull <curto.brull.javier@gmail.com>
2020-08-12 13:23:53 +0200

    Update clase8_parametros_ref_val.js

--- clase8_parametros_ref_val.js
+++ clase8_parametros_ref_val.js
@@ -1,1 +1,20 @@
+var javier = {
+  nombre: 'Javier',
+  apellidos: 'Curto',
+  edad: 41
+}
 
+function cumple(persona) {
+  persona.edad += 1
+}
+// Va incrementando el valor cada vez
+ cumple(javier)
+
+function cumple2(persona) {
+  return {
+    ...persona,
+    edad: persona.edad + 1
+  }
+}
+
+cumple2(javier)
